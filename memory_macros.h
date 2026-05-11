105f71324d5e4cc18926dffee1cb8f4ba138bafc
Roman Horshkov <124358264+RomanHorshkov@users.noreply.github.com>
2026-05-11 20:24:40 +0200

    update time, memory and preproc macros

---
+++ memory_macros.h
@@ -0,0 +1,62 @@
+/**
+ * @file memory_macros.h
+ * @brief Byte-size conversion helpers used by configuration headers.
+ *
+ * This header centralizes small, constexpr-style macros that convert units
+ * such as kibibytes and mebibytes to byte counts expressed as `size_t`.
+ */
+
+#ifndef MEMORY_UNIT_MACROS_H_
+#define MEMORY_UNIT_MACROS_H_
+
+/****************************************************************************
+ * PUBLIC INCLUDES
+ ****************************************************************************
+ */
+#include <stddef.h> /* NULL, size_t */
+
+/**
+ * @brief byte definition
+ *
+ * @param x Size in bytes.
+ *
+ * @return Size in bytes as a `size_t` expression.
+ */
+#ifndef MEMORY_SIZE_B
+#define MEMORY_SIZE_B(x) ((size_t)(x) * 1U) /* define a byte */
+#endif
+
+/**
+ * @brief Convert kibibytes to bytes.
+ *
+ * @param x Size in KiB.
+ *
+ * @return Size in bytes as a `size_t` expression.
+ */
+#ifndef MEMORY_SIZE_KiB_2_B
+#define MEMORY_SIZE_KiB_2_B(x) (MEMORY_SIZE_B(x) * 1024ULL)
+#endif
+
+/**
+ * @brief Convert mebibytes to bytes.
+ *
+ * @param x Size in MiB.
+ *
+ * @return Size in bytes as a `size_t` expression.
+ */
+#ifndef MEMORY_SIZE_MiB_2_B
+#define MEMORY_SIZE_MiB_2_B(x) (MEMORY_SIZE_KiB_2_B(x) * 1024ULL)
+#endif
+
+/**
+ * @brief Convert gibibytes to bytes.
+ *
+ * @param x Size in GiB.
+ *
+ * @return Size in bytes as a `size_t` expression.
+ */
+#ifndef MEMORY_SIZE_GiB_2_B
+#define MEMORY_SIZE_GiB_2_B(x) (MEMORY_SIZE_MiB_2_B(x) * 1024ULL)
+#endif
+
+#endif /* MEMORY_UNIT_MACROS_H_ */
---
+++ time_macros.h
@@ -0,0 +1,59 @@
+/**
+ * @file time_macros.h
+ * @brief conversion helpers.
+ */
+
+#ifndef TIME_UNIT_MACROS_H_
+#define TIME_UNIT_MACROS_H_
+
+/****************************************************************************
+ * PUBLIC INCLUDES
+ ****************************************************************************
+ */
+#include <stddef.h> /* NULL, size_t */
+
+/**
+ * @brief second definition
+ *
+ * @param x Size in seconds.
+ *
+ * @return Size in seconds as a `size_t` expression.
+ */
+#ifndef TIME_SIZE_SECOND
+#define TIME_SIZE_SECOND(x) ((size_t)(x) * 1U)
+#endif
+
+/**
+ * @brief Convert minutes to seconds.
+ *
+ * @param x Size in minutes.
+ *
+ * @return Size in seconds as a `size_t` expression.
+ */
+#ifndef TIME_SIZE_MINUTE_2_SECOND
+#define TIME_SIZE_MINUTE_2_SECOND(x) (TIME_SIZE_SECOND(x) * 60ULL)
+#endif
+
+/**
+ * @brief Convert hours to seconds.
+ *
+ * @param x Size in hours.
+ *
+ * @return Size in seconds as a `size_t` expression.
+ */
+#ifndef TIME_SIZE_HOUR_2_SECOND
+#define TIME_SIZE_HOUR_2_SECOND(x) (TIME_SIZE_MINUTE_2_SECOND(x) * 60ULL)
+#endif
+
+/**
+ * @brief Convert days to seconds.
+ *
+ * @param x Size in GiB.
+ *
+ * @return Size in seconds as a `size_t` expression.
+ */
+#ifndef TIME_SIZE_DAY_2_SECOND
+#define TIME_SIZE_DAY_2_SECOND(x) (TIME_SIZE_HOUR_2_SECOND(x) * 24ULL)
+#endif
+
+#endif /* TIME_UNIT_MACROS_H_ */
